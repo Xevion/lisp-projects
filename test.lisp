2487b3472c1608b6444064d85c96ef64841475d9
Xevion <xevioni@yandex.com>
2020-03-02 00:48:53 -0600

    prompting script

---
+++ test.lisp
@@ -0,0 +1,11 @@
+(defun read-3-numbers-&-format-sum ()
+  (flet ((prompt (string)
+           (format t "~&~a: " string)
+           (finish-output)
+           (read nil 'eof nil)))
+    (let ((x (prompt "first number"))
+          (y (prompt "second number"))
+          (z (prompt "third number")))
+      (format t "~&the sum of ~a, ~a, & ~a is:~%~%~a~%"
+              x y z (+ x y z)))))
+(read-3-numbers-&-format-sum)
